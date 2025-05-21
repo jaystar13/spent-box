d0927b67d5ca8fb548fc7e6082053b747ec2461b
jaystar13 <astarisk2@gmail.com>
2025-05-21 20:27:10 +0900

    chore: 데이터베이스변경

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 
-DB_URL = "sqlite:///./spent-box.db"
+DB_URL = "sqlite:///:memory:"
 
 engine = create_engine(DB_URL, pool_size=50, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
